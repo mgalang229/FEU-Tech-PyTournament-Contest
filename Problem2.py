d3d3c013a1e23463c85250c87ff04322e2a14966
Mark Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-05-21 20:43:08 +0800

    Create Problem2.py

---
+++ Problem2.py
@@ -0,0 +1,10 @@
+def main():
+    d = int(input())
+    ans = []
+    for i in range(d):
+        l1, c1, l2, c2 = map(int, input().split())
+        ans.append(max(l1 * c1, l2 * c2))
+    [print(x) for x in ans]
+
+if __name__ == "__main__":
+    main()
